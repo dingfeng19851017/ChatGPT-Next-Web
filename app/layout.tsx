91d07a8001e46a5a04700060372d47d5602cdc1e
dingfeng19851017 <40775745+dingfeng19851017@users.noreply.github.com>
2023-08-22 18:24:12 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "OpenAI/ChatGPT独享/成品/纯手工永久个人账号购买与出售自动发货平台",
+  description: "在线提供超高质量纯手工chatgpt账号购买与出售& ChatGPT账号购买批发出售Open AI成品号购买平台，ChatGPT 24小时自助购买交易平台.",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "OpenAI/ChatGPT独享/成品/纯手工永久个人账号购买与出售自动发货平台",
     statusBarStyle: "default",
   },
 };
